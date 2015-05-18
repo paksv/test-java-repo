ff6848dcc59c5c3527beeeff103e2dcff2beb6cd
Sergey Pak <paksv@yandex.ru>
2015-05-18 12:58:49 +0300

    initial commit

---
+++ src/main/java/tcdemo/Demo.java
@@ -0,0 +1,11 @@
+package tcdemo;
+
+/**
+ * Created by jetbrains on 18.05.2015.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
