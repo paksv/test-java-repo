527dbc39512580ec3a87ab0edac9a2d228de6d57
Sergey Pak <paksv@yandex.ru>
2015-05-20 10:53:30 +0300

    more files

--- src/main/java/tcdemo/Demo.java
+++ src/main/java/tcdemo/Demo.java
@@ -1,8 +1,5 @@
 package tcdemo;
 
-/**
- * Created by jetbrains on 18.05.2015.
- */
 public class Demo {
 
     public static void main(String[] args) {
---
+++ src/test/java/tcdemo/DemoTest.java
@@ -0,0 +1,15 @@
+package tcdemo;
+
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class DemoTest {
+    public void test1(){
+
+    }
+
+    public void test2_fail(){
+        Assert.fail();
+    }
+}
