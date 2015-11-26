bb16f67a0eba66a04711fbd35ff24a182175e3c7
Sergey.Pak <sergey.pak@jetbrains.com>
2015-11-26 16:00:05 +0300

    updated test

--- plus/src/test/java/tcdemo/PlusTest.java
+++ plus/src/test/java/tcdemo/PlusTest.java
@@ -1,16 +1,25 @@
 package tcdemo;
 
 import junit.framework.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @Test
 public class PlusTest {
-    public void test1(){
-        // test
+
+    private PlusAction myAction;
+
+    @BeforeMethod
+    public void setUp(){
+        myAction = new PlusAction();
+    }
+
+    public void test2_plus_2(){
+        Assert.assertEquals(4, myAction.action(2, 2));
     }
 
 
-    public void test2_fail(){
-        Assert.fail();
+    public void test2_plus_3(){
+        Assert.assertEquals(4, myAction.action(2, 3));
     }
 }
