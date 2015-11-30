fc898766ba0cbf9c787e06386a178bdcb5cd286d
Sergey.Pak <sergey.pak@jetbrains.com>
2015-11-30 15:18:07 +0300

    linux branch created

--- winComparer/src/main/java/tcdemo/Compare.java
+++
@@ -1,8 +0,0 @@
-package tcdemo;
-
-public class Compare {
-
-    public static void main(String[] args) {
-        new Executor().execute(args, new LinuxPathComparer(), Compare.class.getName());
-    }
-}
