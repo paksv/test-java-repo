0604ecd3e3720d81801bcfd56e2cbde3768f449d
Sergey Pak <paksv@yandex.ru>
2018-01-18 13:27:49 +0100

    version: 1

--- src/main/java/tcdemo/Compare.java
+++ src/main/java/tcdemo/Compare.java
@@ -2,6 +2,8 @@ package tcdemo;
 
 public class Compare {
 
+    // version: 1
+
     public static void main(String[] args) {
         new Executor().execute(args, new CompareAction(), Compare.class.getName());
         System.out.println(2);
